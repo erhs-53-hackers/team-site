ded54042d166aaefbc528795cd9706560dd271c2
Michael Stevens <mls0.5mv2@gmail.com>
2012-06-13 21:22:28 -0400

    admin in progress

--- main.py
+++ main.py
@@ -33,11 +33,59 @@ class LoginHandler(Handler):
             if valid_pw(username, password, user[0].password):
                 user_id = make_secure_val(str(user[0].key().id()))
                 self.response.headers.add_header('Set-Cookie', 'user_id=%s' % user_id)
-                self.redirect('/welcome')
+                self.redirect('/')
             else:
-                self.render('login.html',username=username, error = "invalid password.")
+                self.render('login.html',username=username, error = "invalid password")
         else:
-            self.render('login.html',username=username, error = "invalid login.")
+            self.render('login.html',username=username, error = "invalid login")
+            
+class AdminHandler(Handler):
+     def get(self):
+        self.render("admin.html")
+
+     def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        v_user = match(USER, username)
+        v_pass = match(PASS, password)
+        v_verify = None
+        if password == verify:
+            v_verify = 1       
+
+        v_email = 1
+        if email: v_email = match(EMAIL, email)
+
+        v_existing_user = db.GqlQuery('SELECT * FROM User WHERE username=:1', username)
+        v_existing_user = v_existing_user.count()
+
+        if v_user and v_pass and v_verify and v_email and v_existing_user < 1:
+            password = make_pw_hash(username, password)
+            newuser = User(username=username, password=password, email = email, isadmin=False)
+            newuser.put()
+            user_id = make_secure_val(str(newuser.key().id()))
+            self.response.headers.add_header('Set-Cookie', 'user_id=%s' % user_id)
+            self.redirect('/')
+        else:
+            m_user = ''
+            m_pass = ''
+            m_verify = ''
+            m_email = ''
+            if not v_user: m_user = 'not a valid username.'            
+            if not v_pass: m_pass = 'not a valid password.'
+            if not v_verify: m_verify = 'passwords do not match.'
+            if not v_email: m_email = 'not a valid email.'
+            if v_existing_user > 0: m_user = 'That user already exists.'
+            
+            self.render("admin.html", name = username,
+                        email = m_email,
+                        username = m_user,
+                        password =  m_pass,
+                        verify = m_verify,
+                        mail = email)
+
 
     
         
@@ -46,5 +94,6 @@ class LoginHandler(Handler):
         
 
 app = webapp2.WSGIApplication([('/blog', BlogHandler),
-                               ('/login', LoginHandler)],
+                               ('/login', LoginHandler),
+                                ('/admin', AdminHandler)],
                               debug=True)
